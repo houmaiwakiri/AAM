1568bc1af64b65524c2752cc537e8da55aebf9dd
houmaiwakiri <mahomahoma888@gmail.com>
2022-02-03 21:43:49 +0900

    firebase導入

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="post">
+      <div>
+        <form class="post_design" action="" method="post">
+          <h1>投稿欄</h1>
+          <span class="post_title">いつ遊ぶ？:</span>
+          <input type="date" min="2021-1-1" max="2022-3-31" name="when"></input><br></br>
+          <span class="post_title">何で遊ぶ？:</span>
+          <input type="text" name="doit"></input>            
+          <br></br>
+          <div class="input_submit"><input type="submit" name="submit" value="投稿"></input></div>
+        </form> 
+      </div>
+      <div class="p_column">
+        <div class="wrapper">
+          <div class="card">
+            <div class="card-content">
+              <p class="card-title">いつ</p>
+              <p class="card-text">なにするか</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
